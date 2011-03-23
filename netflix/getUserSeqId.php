eafe62d166fb6160a9d16dbb3b115f5e8052aa00
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-03-23 06:32:39 +0000

    利用netflix的数据实现koren TKDD‘09中的算法，目前只是数据预处理

---
+++ netflix/getUserSeqId.php
@@ -0,0 +1,55 @@
+<?php
+$dirBase = '/home/lvhl/nf_dataset/training_set/';
+$dstBase = '/home/lvhl/nf_dataset/transfer_set/';
+//第一步给所有的用户编成一个连续的号码，然后将文件重新保存
+
+$userIds = array();
+$num = 0;
+$userNum = 1;
+//文件名example：mv_0000023.txt
+$posStart = 0;
+$posEnd = 0;
+$movieId = 0;
+$rateContent = $ret = '';
+foreach( glob($dirBase.'*.txt') as $filename)
+{
+	//针对每一个文件，提取出movie的id
+	
+	$posStart = strrpos($filename,'_');
+	$posEnd = strrpos($filename,'.');
+	$movieId = substr($filename,$posStart+1);
+	//echo $movieId;die;
+	//下面给每一个用户编号，先读取文件内容，然后用正则表达式匹配
+	//$rateContent = file_get_contents($filename);
+	//$rateArray = explode("\n",$rateContent);
+	$handler = fopen($dstBase.$movieId,'w');
+	$rateHandler = fopen($filename,'r');
+	while (!feof($rateHandler)) 
+	{
+		$buffer = fgets($rateHandler, 4096);
+		$details = explode(',',$buffer);
+		$userId = '';
+		if(count($details)< 2)continue;
+		if(!isset($userIds[$details[0]]))
+		{
+			$userIds[$details[0]] = $userNum; //给用户编号
+			$userId = $userNum;
+			++$userNum;
+		}
+		else
+		{
+			$userId = $userIds[$details[0]];
+		}
+		fwrite($handler, $userId.','.$details[1].','.$details[2]);
+	}
+	fclose($handler);
+	fclose($rateHandler);
+	echo $filename." is dealed!\n";
+}
+$userIdStr  = '';
+foreach($userIds as $key=>$userId)
+{
+	$userIdStr .= $key."\t".$userId."\n";
+}
+file_put_contents('userIds',$userIdStr);
+?>
---
+++ netflix/mdefine.cpp
@@ -0,0 +1,203 @@
+float test_level = 0.004;
+using namespace std;
+void explode(const char * probe, char * data,vector<string> &result);
+void qsort(vector<double>& array, int start, int end);
+int  partition(vector<double>& array, int start, int end);
+bool cmp(double a, double b);
+Rating::Rating(int u_item,int u_user,float u_rate)
+{
+    item = u_item;
+    user = u_user;
+    rate = u_rate;
+}
+
+double Rating::predict()
+{
+    return 0.0;
+}
+
+void Rating::setPredict(float rating)
+{
+    rate = rating;
+}
+
+float Rating::value()
+{
+    return rate;
+}
+int Rating::year()
+{
+    return yearValue;
+}
+
+int Rating::month()
+{
+    return monthValue;
+    int good =10;
+}
+
+
+float dot(vector<float> &p, vector<float> &q)
+{
+    float result = 0.0;
+    for (int i=0; i<p.size(); ++i){
+        result += p[i]*q[i];
+    }
+    return result;
+}
+
+/**
+ * load filePath中的数据到data这个vector中和 rateMatrix中
+ */
+void loadRating(char * filePath, vector<Rating> &data, map<int,int> rateMatrixLocal[])
+{
+    std::ifstream from (filePath);
+    char rateStr[256];
+    vector<string> rateDetail(4);
+   
+    while(from.getline(rateStr,256)){
+        explode("	",rateStr,rateDetail);
+        if(rateDetail.size()>=3){
+        	Rating aRate(atoi(rateDetail[1].c_str()),atoi(rateDetail[0].c_str()),atof(rateDetail[2].c_str()));
+			aRate.test = 0; //这里所有的数据都是训练数据，测试数据另外存储        	
+        	data.push_back(aRate);
+        	
+        	//初始话rateMatrix
+        	rateMatrixLocal[atoi(rateDetail[0].c_str())][atoi(rateDetail[1].c_str())] = atoi(rateDetail[2].c_str());
+        }
+    }
+    from.close();
+    return;
+}
+
+void explode(const char * probe,  char * data ,vector<string> &result)
+{
+    string dataStr(data);
+    int pos1 = 0;
+    int pos2 = 0;
+    int i=0;
+    while((pos2=dataStr.find(probe,pos1)) != string::npos){
+        result[i] = (dataStr.substr(pos1,pos2-pos1));
+        ++i;
+        //if(i>=3)break;
+        pos1=pos2+1;
+    }
+    result[i] = (dataStr.substr(pos1));
+}
+
+/**
+ * 计算全局的平均值
+ */
+int setMeanRating(vector<Rating> &data)
+{
+    //计算平均值;
+    float sum = 0;
+    int num = 0;
+    int tmp = 0;
+    for(int i = 0; i < data.size(); ++i){
+    	tmp = data[i].value();
+    	if(data[i].value() > 0){
+    		sum += data[i].value();
+    		++num;
+    	}  			
+    }
+    return sum/num;
+}
+
+double get_rand()
+{
+    return (rand()%1000-500)/5000.0;
+}
+
+/**
+ * setRand的值
+ */
+float setRand(vector<double> &p, int num, float base)
+{
+	srand((unsigned)time(0));
+    for(int i=0;i<num;++i){
+    	double temp = base+get_rand();
+        p.push_back(temp);
+        //p.push_back(base+0.05); //全部初始化为base，看看影响如何
+        //cout << i <<"	"<< temp <<"	"<< endl;
+    }
+}
+
+
+
+
+void ratingProbe(const char * filePath, vector<Rating> &data)
+{
+
+}
+
+void saveRating(const char * filePath, vector<Rating> &data)
+{
+}
+
+void ratingQuiz(const char * filePath, vector<Rating> &data)
+{
+}
+
+
+//下面的这个函数用来利用最小堆找出第k大的元素
+double getKmax(vector<double>& array, int K)
+{
+	vector<double> heapTmp;
+	for(int i=0; i < array.size(); ++i)
+	{
+		heapTmp.push_back(array[i]);
+		if(i == K-1) {
+			make_heap(heapTmp.begin(),heapTmp.end(),cmp);
+		}
+		else if(i >=K) {
+			push_heap(heapTmp.begin(),heapTmp.end(),cmp);
+			pop_heap(heapTmp.begin(),heapTmp.end(),cmp);
+			heapTmp.pop_back();
+		}
+		//cout << i<<'\t'<<heapTmp.size()<<endl;
+	}
+	return heapTmp.front();
+}
+
+bool cmp(double a, double b)
+{
+	return a > b;
+}
+
+//下面的几个函数用来实现快速排序
+void qsort(vector<double>& array, int start, int end)
+{
+    if(start >= end)return;
+    //cout<<"begin partition:"<<endl;
+    int q = partition(array,start,end);
+    //cout<< q<<endl;
+    qsort(array,start,q-1);
+    qsort(array,q+1,end);
+}
+
+int partition(vector<double>& array, int start,int end)
+{
+    if(start >= end) return start;
+    if(start < 0) start = 0;
+    int original  = start;
+    double tmp = array[start];
+    ++start;
+    while(1) {
+        while( start<=end && array[start] >= tmp)++start;
+        while( start<=end && array[end] < tmp)--end;
+        if(start < end) {
+        	swap(array[start],array[end]);
+        }
+        else break;
+    }
+    swap(array[start-1],array[original]);
+    return start-1;
+}
+
+void swap(double& a, double& b)
+{
+    double tmp = a;
+    a = b;
+    b = tmp;
+}
