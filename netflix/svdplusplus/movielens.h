147a2ba0ed6ec927a457afa0626bb1ba8cb96ba7
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-03-31 09:30:44 +0000

    实现netflix数据的svdplusplus版本，比较一些我的结果和koren的结果是否一致。

---
+++ netflix/svdplusplus/movielens.h
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <stdlib.h>
+#include <algorithm>
+#include <ctime>
+#include <dirent.h>
+#include <malloc.h>
+#include <errno.h>
+
+//#define USER_NUM 438212 
+//#define ITEM_NUM 1000
+#define USER_NUM 480189 
+#define ITEM_NUM 17770
+#define K_NUM  5
+#define DIR_PATH "../../nf_dataset/transfer_set/"
+#define PROBE_SET "../probe_real.txt"
+#define TRAINING_SET "../data_without_prob.txt"
+
+class Rating
+{
+    public:
+        Rating(short u_item,int u_user,short u_rate);//,int year,int month,int day);
+        short item;
+        int user;
+        short rate;
+        //int test;
+        //float real;
+        //double predict();
+        //int yearValue;
+        //int monthValue;
+        //int dayValue;
+        void setPredict(int rating);
+        short value();
+        //int   year();
+        //int   month();
+    private:
+        ;
+};
+
+
+struct rateNode
+{
+	short item;
+	short rate;
+};
