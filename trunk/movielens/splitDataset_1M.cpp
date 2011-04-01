c68c76b775f32c5f9602c536ebbef00fe7d40131
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-04-01 08:01:11 +0000

    add a splitor to split the 1M movielens dataset to training set and test set

---
+++ trunk/movielens/splitDataset_1M.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#define  SOURCE "../ml10M_data/ratings.dat"
+using namespace std;
+
+int main()
+{
+    ifstream in(SOURCE);
+    char str[200];
+    while(in.getline(str) {
+    
+          }
+}
