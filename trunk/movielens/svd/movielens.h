9ef51bdd17e4757f014b5738536c8ecc27b85e63
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-04-02 15:15:34 +0000

    add bu and bi , change the Convergence condition(use the change of the RMSE of probe set)

--- trunk/movielens/svd/movielens.h
+++ trunk/movielens/svd/movielens.h
@@ -31,10 +31,10 @@
 //#define ITEM_NUM 1000
 #define USER_NUM 6040  //10K:943 1M:6040
 #define ITEM_NUM 3706 //10K:1682 1M:3900
-#define K_NUM  100
+#define K_NUM  30
 #define DIR_PATH "../../nf_dataset/transfer_set/"
-#define PROBE_SET "../../ml_data/test.txt"
-#define TRAINING_SET "../../ml_data/training.txt"
+#define PROBE_SET "../../ml_data/ua.test"
+#define TRAINING_SET "../../ml_data/ua.base"
 using namespace std;
 
 struct rateNode
