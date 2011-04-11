0387ff8f05a280b125bbdb5f13b05f7ed4c2370f
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-04-11 07:34:48 +0000

    delete some unused Files

--- mcriteria/preProcess/getUserIdHotelId.php
+++ mcriteria/preProcess/getUserIdHotelId.php
@@ -1,14 +1,30 @@
 ﻿<?php
-$dirBase = 'F:\dataset\tripadvisor\Texts';
+/*
+* This file is Copyright (C) 2011 Lv Hongliang. All Rights Reserved.
+* please maitain the copyright information completely when you redistribute the code.
+* 
+* Please contact me via email honglianglv@gmail.com
+* my blog: http://lifecrunch.biz
+* my twitter: http://twitter.com/honglianglv
+*
+* It is free software; you can redistribute it and/or modify it under 
+* the GNU General Public License as published by the Free Software
+* Foundation; either version 1, or (at your option) any later version.
+* 
+* 给所有酒店编号，给所有的用户编号，编号的规则存入两个文件 hotelIds 和 userIds
+*/
+//var_dump(unserialize(file_get_contents(hotelIds)));
+//var_dump(unserialize(file_get_contents(userIds));
+//die;
+$dirBase = '../tripDataset';
 
-//第一步是不是应该给所有的酒店和所有的用户编上号？恩，是应该这样。
-//给所有酒店编号，给所有的用户编号，编号的规则存入两个文件 hotelIds 和 userIds
+
 //(1)给酒店编号
 $hotelIds = array();
 $userIds = array();
-$num = 0;
-$userNum = 0;
-foreach( glob($dirBase.'\*.txt') as $filename)
+$num = 1;
+$userNum = 1;
+foreach( glob($dirBase.'/*.txt') as $filename)
 {
 	//针对每一个文件，提取出酒店的id
 	$posStart = strpos($filename,'_');
@@ -29,8 +45,18 @@ foreach( glob($dirBase.'\*.txt') as $filename)
 		}
 	}
 }
-$hotelIdStr = serialize($hotelIds);
-$userIdStr  = serialize($userIds);
+$hotelIdStr = '';
+foreach($hotelIds as $key=>$hotelId) {  //将hotelId储存成c容易读取的形式
+	$hotelIdStr .= $key."\t".($hotelId)."\n";
+}
+$userIdStr = '';
+foreach($userIds as $key=>$userId) {  //将hotelId储存成c容易读取的形式
+	$userIdStr .= $key."\t".($userId)."\n";
+}
+//$hotelIdStr = serialize($hotelIds);
+//$userIdStr  = serialize($userIds);
 file_put_contents('hotelIds',$hotelIdStr);
 file_put_contents('userIds',$userIdStr);
+file_put_contents('hotelIdArrays',serialize($hotelIds));
+file_put_contents('userIdArrays',serialize($userIds));
 ?>
