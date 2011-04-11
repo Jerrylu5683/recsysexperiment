a9990eae6e9fcdf23574182828dc897c422e12e0
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-04-11 14:04:46 +0000

    finish the preprocess of the data,but the data is too sparse

--- trunk/mcriteria/preProcess/getUserIdHotelId.php
+++ trunk/mcriteria/preProcess/getUserIdHotelId.php
@@ -12,10 +12,8 @@
 * Foundation; either version 1, or (at your option) any later version.
 * 
 * 给所有酒店编号，给所有的用户编号，编号的规则存入两个文件 hotelIds 和 userIds
+*
 */
-//var_dump(unserialize(file_get_contents(hotelIds)));
-//var_dump(unserialize(file_get_contents(userIds));
-//die;
 $dirBase = '../../tripDataset_o'; 
 
 
@@ -27,16 +25,19 @@ $userNum = 1;
 foreach( glob($dirBase.'/*.txt') as $filename)
 {
 	//针对每一个文件，提取出酒店的id
-	$posStart = strpos($filename,'_');
-	$posEnd = strpos($filename,'_',$posStart+1);
-	$hotelId = substr($filename,$posStart+1,$posEnd - $posStart-1);
+	$nameArray = explode('/',$filename);
+	$fileSub = $nameArray[count($nameArray)-1];
+	$posStart = strpos($fileSub,'_');
+	$posEnd = strpos($fileSub,'_',$posStart+1);
+	$hotelId = substr($fileSub,$posStart+1,$posEnd - $posStart-1);
 	$hotelIds[$hotelId] = $num;
 	++$num;
 	//下面给每一个用户编号，先读取文件内容，然后用正则表达式匹配
 	$rateContent = file_get_contents($filename);
 	preg_match_all('/<Author>(.+)/i',$rateContent,$usersArray);
-	foreach($usersArray[1] as $user)
-	{
+	foreach($usersArray[1] as $user)		{
+
+		$user = trim($user);
 		if('A TripAdvisor Member' == $user)continue;
 		if(!isset($userIds[$user]))
 		{
@@ -49,15 +50,15 @@ $hotelIdStr = '';
 foreach($hotelIds as $key=>$hotelId) {  //将hotelId储存成c容易读取的形式
 	$hotelIdStr .= $key."\t".($hotelId)."\n";
 }
-var_dump($hotelIds);
 $userIdStr = '';
 foreach($userIds as $key=>$userId) {  //将hotelId储存成c容易读取的形式
 	$userIdStr .= $key."\t".($userId)."\n";
 }
 //$hotelIdStr = serialize($hotelIds);
 //$userIdStr  = serialize($userIds);
-file_put_contents('hotelIds',$hotelIdStr);
-file_put_contents('userIds',$userIdStr);
-file_put_contents('hotelIdArrays',serialize($hotelIds));
-file_put_contents('userIdArrays',serialize($userIds));
+file_put_contents('tmpData/hotelIds',$hotelIdStr);
+file_put_contents('tmpData/userIds',$userIdStr);
+file_put_contents('tmpData/hotelIdArrays',serialize($hotelIds));
+file_put_contents('tmpData/userIdArrays',serialize($userIds));
+echo "get userId Map and hotelId map successfully!\n";
 ?>
