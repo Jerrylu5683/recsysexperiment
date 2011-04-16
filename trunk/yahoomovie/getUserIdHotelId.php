36d718f9e090277403fab3a4d889fd8a9110103f
honglianglv <honglianglv@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2011-04-16 09:47:56 +0000

    stat the rate num of items and users

--- trunk/yahoomovie/getUserIdHotelId.php
+++ trunk/yahoomovie/getUserIdHotelId.php
@@ -7,46 +7,62 @@
 * my blog: http://lifecrunch.biz
 * my twitter: http://twitter.com/honglianglv
 *
-* It is free software; you can redistribute it and/or modify it under 
-* the GNU General Public License as published by the Free Software
-* Foundation; either version 1, or (at your option) any later version.
-* 
-* 
-*
+* It is free software; you can redistribute it and/or modify it under GPLV3.
 */
+include "./common.php";
+
 $filename = './clean-yahoo-user-ratings.txt'; 
 
-$movieIds = array();
+$itemIds = array();
 $userIds = array();
-$num = 1;
+$userStat = array(); //stat the rate num of every user
+$itemStat = array(); //stat the rate num of every item
+$itemNum = 1;
 $userNum = 1;
 
 $rateContent = file_get_contents($filename);
 $rateArray = explode("\n",$rateContent);
-echo count($rateArray);die;
-foreach($usersArray[1] as $user)		{
 
-	$user = trim($user);
-	if('A TripAdvisor Member' == $user)continue;
-	if(!isset($userIds[$user]))
-	{
-		$userIds[$user] = $userNum; //给用户编号
-		++$userNum;
-	}
+foreach($rateArray as $rate){
+    if(strlen($rate) < 5)continue;
+    $rateDetail = explode(" ",trim($rate));
+    //itemId, UserId, StoryRating, ActingRating, DirectionRating, VisualRating, OverallRating
+    $userId = trim($rateDetail[1]);
+    $itemId = trim($rateDetail[0]);
+    
+    if(!isset($userIds[$userId]))
+    {
+        $userIds[$userId] = $userNum; //给用户编号
+        $userStat[$userNum] = 1;
+        ++$userNum;
+        
+    }
+    else ++$userStat[$userIds[$userId]];
+    
+    if(!isset($itemIds[$itemId]))
+    {
+        $itemIds[$itemId] = $itemNum; //给用户编号
+        $itemStat[$itemNum] = 1;
+        ++$itemNum;
+    }
+    else ++$itemStat[$itemIds[$itemId]];
 }
-$movieIdStr = '';
-foreach($movieIds as $key=>$movieId) {  //将movieId储存成c容易读取的形式
-	$movieIdStr .= $key."\t".($movieId)."\n";
+$itemIdStr = '';
+foreach($itemIds as $key=>$itemId) {  //将itemId储存成c容易读取的形式
+    $itemIdStr .= $key."\t".($itemId)."\n";
 }
 $userIdStr = '';
-foreach($userIds as $key=>$userId) {  //将movieId储存成c容易读取的形式
-	$userIdStr .= $key."\t".($userId)."\n";
+foreach($userIds as $key=>$userId) {  //将itemId储存成c容易读取的形式
+    $userIdStr .= $key."\t".($userId)."\n";
 }
-//$movieIdStr = serialize($movieIds);
-//$userIdStr  = serialize($userIds);
-file_put_contents('tmpData/movieIds',$movieIdStr);
+asort($userStat); //对user打分数量进行排序
+asort($itemStat); //对item打分数量进行排序
+
+file_put_contents('tmpData/itemIds',$itemIdStr);
 file_put_contents('tmpData/userIds',$userIdStr);
-file_put_contents('tmpData/movieIdArrays',serialize($movieIds));
+saveArrayToFile($userStat,'tmpData/userStat');
+saveArrayToFile($itemStat,'tmpData/itemStat');
+file_put_contents('tmpData/itemIdArrays',serialize($itemIds));
 file_put_contents('tmpData/userIdArrays',serialize($userIds));
-echo "get userId Map and movieId map successfully!\n";
+echo "get userId Map and itemId map successfully!\n";
 ?>
