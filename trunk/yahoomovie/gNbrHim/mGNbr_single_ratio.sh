9552188f0fdc50b14ab6b6e4bb88688ab751cf76
honglianglv@gmail.com <honglianglv@gmail.com@0a3c324c-3bb5-3624-a523-737c14b5ab86>
2012-01-20 07:48:39 +0000

    finish all the experimens

--- trunk/yahoomovie/gNbrHim/mGNbr_single_ratio.sh
+++ trunk/yahoomovie/gNbrHim/mGNbr_single_ratio.sh
@@ -2,7 +2,7 @@
 #this file use to get one result of mGNbr model(the muliti-criteria global neighborhood model)
 #just input the ratio and then the programme will split the data by ratio and then give the predict values of testSet.
 #
-ratio=0.2
+ratio=$1
 echo "The ratio of testSet is: "$ratio
 cd /home/users/lvhongliang/recsysexperiment/yahoomovie
 ./splitRatings.sh ../yahooMovie/yahooMovie.data  $ratio #split the ratings
